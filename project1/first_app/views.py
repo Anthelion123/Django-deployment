c8b1710e4da72b3a15754186816ed647cf356588
余昭熙 <muriel@yuzhaoxideMacBook-Pro.local>
2020-04-11 20:04:08 -0700

    first

---
+++ project1/first_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
+# Create your views here.
+
+def index(request):
+    webpg_list = AccessRecord.objects.order_by('date')
+    date_dict = {'acc_rec':webpg_list}
+
+    # my_dict = {'insert_me':'Hello I am from first_app/index.html'}
+    return render(request,'first_app/index.html',context=date_dict)
